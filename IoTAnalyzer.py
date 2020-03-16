84afcc5652f37cee32df239b8205b14c49c54f70
Stephen Niemann <step.niemann@web.de>
2020-03-16 01:34:34 +0100

    Implementation of CleartextFinder, implementation of the average entropy

--- IoTAnalyzer.py
+++ IoTAnalyzer.py
@@ -23,7 +23,7 @@ def fillDictWithLocation(ipList):
     '''takes an existing list of ips and looksup the ips and fills a dictionary with the ips and their location information'''
     storrage= {}
     for ip in ipList:
-        if ((ip != '255.255.255.255') and not (re.search('^192.168.', ip)) and not (ip != '0.0.0.0')):  #checks if the data goes to the broadcasting address or stays in the local network
+        if not((ip == '255.255.255.255') or (re.search('^192.168.', ip)) or (ip == '0.0.0.0')):  #checks if the data goes to the broadcasting address or stays in the local network
             jsonCashed = lookupIP(ip)
             storrage[ip] = {'country': jsonCashed['country_name'],
                             'region_name': jsonCashed['region_name'],
@@ -143,13 +143,10 @@ def createCleartextDictionary(packets):
                         clearTextDictionary[oriIp] = {"targetIp": {destIp: result}}
                 except:
                     pass
-                finally:
-                    pass
-            else:
-                print(packet.summary())
     return clearTextDictionary
 
 def entropy(data):
+    '''calculates the entropy of the incoming string'''
     e = 0
     counter = collections.Counter(data)
     #print("counter ", counter)
@@ -161,46 +158,58 @@ def entropy(data):
     return e
 
 def prepareEntropyDic(dict, entropy, oriIp, destIp):
+    '''responsible for the creation of the entropy dictionary '''
     if (oriIp in dict.keys()):
         if (destIp in dict[oriIp].keys()):
             dict[oriIp][destIp]["amount"] += 1
-            dict[oriIp][destIp]["averageEntropy"] += entropy  # calculates the averrage entropy between the two IPs
+            dict[oriIp][destIp]["averageEntropy"] += entropy                                                            # adds up the entropy which will be further calculated to the average within "finishEntropy" method
         else:
-            dict[oriIp][destIp] = {"averageEntropy": entropy, "amount": 1}
+            dict[oriIp][destIp] = {"averageEntropy": entropy, "amount": 1}                                              #creates new dictionary entry if destination with corresponding origin IP is not in yet
     else:
-        dict[oriIp] = {destIp: {"averageEntropy": entropy, "amount": 1}}
+        dict[oriIp] = {destIp: {"averageEntropy": entropy, "amount": 1}}                                                #creates new dictionary entry if origin IP is not in yet
     return dict
 
 def finishEntropyDic(dict):
-    print(dict)
+    '''calculates the average entropy based on the data of the incomming dictionary'''
     for o in dict:
         for d in (dict[o].keys()):
             dict[o][d]["averageEntropy"] /= dict[o][d]["amount"]                                                        # calculates the averrage entropy between the two IPs
     return dict
 
-
 def stepZeroEntropy(packets, upperLimit = 9, lowerLimit = 0):
-    sessions = packets.sessions()
-    amountRaw = 0
-    amountdone = 0
-    amountdonesemi = 0
-    amountdoneExcluded = 0
-    amount = 0
-    entropyDic = {}
+    '''start for entropy calculation
+    prints an overview how many packets are used of ARP, DHCP, DNS, ACK, SYN, SYN/ACK, FA
+    iterates over incoming packets and calculates the average entropy which will be given back in a dictionary
+    upperLimit and lowerLimit allow to define the span of entropy which should be saved'''
+    amountRaw = 0                                                                                                       #for the amount of packets which have a RAW layer
+    amountdone = 0                                                                                                      #for the amount of packets which have utf8 based payload
+    amountdonesemi = 0                                                                                                  #for the amount of packets which have byte which is not utf8 based payload
+    amountdoneExcluded = 0                                                                                              #for the amount of packets outside the given span
+    amount = 0                                                                                                          #for the amount packets total
+    entropyDic = {}                                                                                                     #will store the IPs with their average entropy later on
+    ackCounter = 0                                                                                                      #for the amount of packets used for ACK, SYN, SYN/ACK, RA
+    arpCounter = 0                                                                                                      #for the amount of packets used for ARP, DHCP or DNS
 
-    """for session in sessions:
-        for packet in sessions[session]:"""
     for packet in packets:
-            amount += 1
+            amount += 1                                                                                                 #total amount packets counter
+
+            #counting for the layers start
+            if(packet.getlayer("TCP")):
+                if((packet.getlayer("TCP").flags ==  "A") or (packet.getlayer("TCP").flags == "S") or (packet.getlayer("TCP").flags == "FA") or (packet.getlayer("TCP").flags == "SA") or (packet.getlayer("TCP").flags == "RA")):
+                    ackCounter+=1
+            if(packet.getlayer("ARP") or (packet.getlayer("DNS")) or (packet.getlayer("DHCP"))):
+                arpCounter +=1
+            #counting for the layers end
+
             if (packet.getlayer("Raw")):
-                destIp = packet.getlayer("IP").dst  # retrieves the destination ip of the packet from the IP layer
-                oriIp = packet.getlayer("IP").src  # retrieves the ip from the packet is sent
+                destIp = packet.getlayer("IP").dst                                                                      # retrieves the destination ip of the packet from the IP layer
+                oriIp = packet.getlayer("IP").src                                                                       # retrieves the ip from the packet is sent
                 if (entropy(packet.getlayer('Raw').load) > lowerLimit and entropy(packet.getlayer('Raw').load) < upperLimit):
                     try:
                         impText = packet.getlayer('Raw').load.decode().strip()
                         payload = re.sub(r'[\W+|_]', '',impText)                                                        # \W+ deletes all special characters, \d+ deletes all numbers, |_ deletes all underscores
                         ent = entropy(payload)                                                                          #entropy UTF-8 based without special character
-                        # print(entropy(packet.getlayer('Raw').load))                                                #entropy based on rawdata
+                        # print(entropy(packet.getlayer('Raw').load))                                                   #entropy based on rawdata
                         amountdone += 1
                         entropyDic = prepareEntropyDic(entropyDic, ent, oriIp, destIp)
                     except:
@@ -213,61 +222,73 @@ def stepZeroEntropy(packets, upperLimit = 9, lowerLimit = 0):
                         except:
                             print("something went wrong")
                             print("NEXT FILE")
-                        # print(packet.getlayer('Raw').load.decode())
-                        # print(packet.lastlayer().load)
                 else:
                     amountdoneExcluded += 1
-                    pass
             else:
                 amountRaw += 1
-                pass
+                #analyzation area start: (purpose of the area) to find packets where the payload couldn´t be extracted
+                if (packet.getlayer("TCP")):
+                    if not ((packet.getlayer("TCP").flags ==  "A") or (packet.getlayer("TCP").flags == "S")
+                        or (packet.getlayer("TCP").flags == "FA") or (packet.getlayer("TCP").flags == "SA")
+                        or (packet.getlayer("TCP").flags == "RA")):
+                        packet.show()
+                else:
+                    if (packet.getlayer("ARP") or packet.getlayer("DNS") or packet.getlayer("DHCP")):
+                        pass
+                    else:
+                        #packet.show()
+                        pass
+                #analyzation area ends
 
-    finishEntropyDic(entropyDic)
-    print("amount packets total", amount)
-    print("amount no Raw level", amountRaw)
-    print("amount done byte based", amountdonesemi)
-    print("amount done utf8 based", amountdone)
-    print("amount done utf8 based but to high or low", amountdoneExcluded)
+    finishEntropyDic(entropyDic)                                                                                        #to start the "average" calculation within the entropyDictionary
+    print("amount packets total:", amount)
+    print("amount no Raw level:", amountRaw)
+    print("amount done byte based:", amountdonesemi)
+    print("amount done utf8 based:", amountdone)
+    print("amount done utf8 based but to high or low:", amountdoneExcluded)
+    print("packets used for ACK, SYN, SYN/ACK, FA:", ackCounter)
+    print("packets used for ARP, DHCP or DNS:", arpCounter)
     print("total: ", amountdone + amountdonesemi + amountRaw + amountdoneExcluded)
     return entropyDic
 
-
-
 def stepZeroDictBuildBothIps(packets, portWhiteList):
     '''takes the the packets and the portWhiteList to create the IP dicitonary and the port dictionary'''
-    print("start building dictionary")
+    print("start building IP dictionary")
     dictionary = {}
     portDictionary = {}
     #protocolList = set()
 
+    #analyzation area start: (purpose:) to find specific packages
     """ for p in packets:
         if (p.getlayer("TCP")):
             if(p.getlayer("TCP").chksum == 0x343b):
                 print(p.getlayer("TCP").show())
                 print(p.getlayer("TCP").chksum)
                 """
+    #analyzation area end
 
     for p in packets:
         if (p.getlayer("IP")):  # checks if the packet uses the IP layer
-            # if ((p.getlayer("IP").dst != '255.255.255.255') and not (re.search('^192.168.', p.getlayer("IP").dst))):  #checks if the data goes to the broadcasting
+            # if ((p.getlayer("IP").dst != '255.255.255.255') and not (re.search('^192.168.', p.getlayer("IP").dst))):  # checks if the data goes to the broadcasting
                                                                                                                         # address or stays in the local network
-            destIp = p.getlayer("IP").dst  # retrieves the destination ip of the packet from the IP layer
-            oriIp = p.getlayer("IP").src  # retrieves the ip from the packet is sent
+            destIp = p.getlayer("IP").dst                                                                               # retrieves the destination ip of the packet from the IP layer
+            oriIp = p.getlayer("IP").src                                                                                # retrieves the ip from the packet is sent
             dictionary = buildDictWithBothIp(dictionary, oriIp, destIp)
 
-            if (p.getlayer("TCP")):  # checks if the packet uses the TCP layer
+            if (p.getlayer("TCP")):                                                                                     # checks if the packet uses the TCP layer
                 dictionary = addPorts(dictionary,p, portWhiteList, oriIp)
                 portDictionary = createPortDict(portDictionary, p)
             #protocolList.add(p.getlayer("IP").proto)
 
-    print("finished building the dictionary")
+
     """    print(protocolList)
     for t in protocolList:
         print(getProtocolName(t))"""
 
     sortedPortDictionary = {}
-    for i in sorted(portDictionary):                                    #sorts the dictionary based on the keys
+    for i in sorted(portDictionary):                                                                                    #sorts the dictionary based on the keys
         sortedPortDictionary.update({i : portDictionary[i]})
+    print("finished building the IP dictionary")
     return (dictionary, sortedPortDictionary)
 
 def startTimer ():
@@ -282,8 +303,8 @@ def end(start):
     print("program finished after: ", stop - start)
 
 def main():
-    start = startTimer()                                                #creates a timestamp for the startingtime
-    # filename = input("Please enter the name of the pcap file: ")      #to enter the filename during runtime
+    start = startTimer()                                                                                                #creates a timestamp for the startingtime
+    # filename = input("Please enter the name of the pcap file: ")                                                      #to enter the filename during runtime
     filename = 'withings_monitor_merge.pcap'
     #filename = 'camera.pcap'
     packets = loadfile(filename)                                        #holds the data of pcap file
@@ -293,13 +314,14 @@ def main():
     ipDictionary, sortedPortDictionary = stepZeroDictBuildBothIps(packets, portWhiteList) #returns a dictionaries with IPs and their destination IPs to which they communicate and a dictionary with all used ports and their amount used
     locationSet = buildLocationSet(locationSet,ipDictionary)            #holds a list with all IPs (destinations an origins)
     locationDictionary = fillDictWithLocation(locationSet)              #holds a dictionaries with all IPs and their geolocation informations
+    entropyDictionary = stepZeroEntropy(packets)                        #holds a dictionary with all IPs and their destinations and the average entropy of the exchanged packets
     cleartextDictionary = createCleartextDictionary(packets)            #holds a dictionary with all with all IP who sent cleartext with the corresponding destination address and the cleartext
-    entropyDictionary = stepZeroEntropy(packets)
+
 
     print("Overview of all used Ports and call amounts: ", sortedPortDictionary)
     print("Overview of origin and destination IPs: ", ipDictionary)
     print("Overview of IPs and their geolocation: ", locationDictionary)
-    print("Overview of IPs their send cleartext and the destination IP: ", cleartextDictionary)
+    #print("Overview of IPs their send cleartext and the destination IP: ", cleartextDictionary)
     print("Overview of IPs their destination and the average entropy: ", entropyDictionary)
     end(start)
 
